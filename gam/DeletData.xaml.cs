1fcb44e905461afe113390b7b3aac029bc0ad7f6
44ert <162302457+44ert@users.noreply.github.com>
2024-04-25 13:39:53 +0300

    Update DeletData.xaml.cs

--- gam/DeletData.xaml.cs
+++ gam/DeletData.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +12,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace gam
-{
-    /// <summary>
-    /// Логика взаимодействия для DeletData.xaml
-    /// </summary>
+{ 
     public partial class DeletData : Page
-    {
+    { 
+        SqlDataAdapter adapter;
+        DataTable AeroportTable;
+        SqlConnection connection = null;
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+       
+    
         public DeletData()
         {
             InitializeComponent();
@@ -26,7 +33,29 @@ namespace gam
 
         private void del_btn_Click(object sender, RoutedEventArgs e)
         {
+            string sql = $"delete * from Airplanes where '{ID_DEL}' = airplane_id ";
 
+            AeroportTable = new DataTable();
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+                adapter = new SqlDataAdapter(command);
+                MessageBox.Show("Данные добавлены");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+                if (connection != null)
+                    connection.Close();
+            }
         }
     }
 }
